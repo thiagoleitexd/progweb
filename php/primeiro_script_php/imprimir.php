b089bde5feea49b2140c72e2af1c165f44fc9b1a
Thiago <thiagoleitexd@gmail.com>
2015-06-24 17:16:36 -0400

    primeiro script php

---
+++ php/primeiro_script_php/imprimir.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title> Minha Primeira Pagina !!!! </title>
+<meta charset = "utf-8" />
+</head>
+
+<body>
+
+<?php
+
+echo $_GET["nome"];
+echo "</br>";
+echo $_GET["sexo"];
+echo "</br>";
+echo $_GET["comentarios"];
+echo "</br>";
+
+?>
+
+
+</body>
+
+</html>
